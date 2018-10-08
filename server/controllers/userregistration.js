05e82a18aa5005685010f6d3da6cf40a9d78fa63
osharif12 <sharifo21@yahoo.com>
2018-10-08 12:32:17 -0700

    Commit 1, finished with login API. Continuing to work on other APIs

--- server/controllers/userregistration.js
+++ server/controllers/userregistration.js
@@ -1,67 +1,67 @@
-const user_details = require('../models').User_Details;
-
+const user_details = require("../models").User_Details;
 
 module.exports = {
-	create(req, res) {
-		return user_details
-		.create({
-			FirstName : req.body.FirstName,
-			LastName : req.body.LastName,
-			StreetAddress : req.body.StreetAddress,
-			City : req.body.City,
-			State : req.body.State,
-			Zipcode : req.body.Zipcode,
-			UserName : req.body.UserName,
-			Password : req.body.Password,
-			//Password : bcrypt.hash(req.body.Password, 10),
-		})
-		.then(user_details => res.status(200).send(user_details))
-		.catch(error => res.status(200).send(error)); 
-	},
-	retrieve(req, res) {
+  create(req, res) {
     return user_details
-    .findById(req.params.userid)
-    .then(user_details => {
-      if (!user_details) {
-        return res.status(200).send({
-          message: 'User Not Found',
-        });
-      }
-      return res.status(200).send(user_details);
-    })
-    .catch(error => res.status(400).send(error));
-   },
-   update(req, res) {
-     return user_details
-    .find({
-        where: {
-          id: req.params.userid,
-        },
+      .create({
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        StreetAddress: req.body.StreetAddress,
+        City: req.body.City,
+        State: req.body.State,
+        Zipcode: req.body.Zipcode,
+        UserName: req.body.UserName,
+        //Password : req.body.Password,
+        Password: bcrypt.hashSync(req.body.Password, 8)
+        // var hashedPassword = bcrypt.hashSync(req.body.password, 8);
       })
-    .then(user_details => {
-      if (!user_details) {
-        return res.status(200).send({
-          message: 'Todo Not Found',
-        });
-      }
-      return user_details
-        .update({
-		    FirstName : req.body.FirstName || user_details.FirstName,
-			LastName : req.body.LastName || user_details.LastName,
-			StreetAddress : req.body.StreetAddress || user_details.StreetAddress,
-			City : req.body.City || user_details.City,
-			State : req.body.State || user_details.State,
-			Zipcode : req.body.Zipcode || user_details.Zipcode,
-			UserName : req.body.UserName || user_details.UserName,
-			Password : req.body.Password || user_details.Password,
-        })
-        .then(() => res.status(200).send(user_details))  // Send back the updated todo.
-        .catch((error) => res.status(400).send(error));
-    })
-    .catch((error) => res.status(400).send(error));
-},
+      .then(user_details => res.status(200).send(user_details))
+      .catch(error => res.status(200).send(error));
+  },
+  retrieve(req, res) {
+    return user_details
+      .findById(req.params.userid)
+      .then(user_details => {
+        if (!user_details) {
+          return res.status(200).send({
+            message: "User Not Found"
+          });
+        }
+        return res.status(200).send(user_details);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  update(req, res) {
+    return user_details
+      .find({
+        where: {
+          id: req.params.userid
+        }
+      })
+      .then(user_details => {
+        if (!user_details) {
+          return res.status(200).send({
+            message: "Todo Not Found"
+          });
+        }
+        return user_details
+          .update({
+            FirstName: req.body.FirstName || user_details.FirstName,
+            LastName: req.body.LastName || user_details.LastName,
+            StreetAddress: req.body.StreetAddress || user_details.StreetAddress,
+            City: req.body.City || user_details.City,
+            State: req.body.State || user_details.State,
+            Zipcode: req.body.Zipcode || user_details.Zipcode,
+            UserName: req.body.UserName || user_details.UserName,
+            Password: req.body.Password || user_details.Password
+          })
+          .then(() => res.status(200).send(user_details)) // Send back the updated todo.
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  }
 };
-		/*.find({
+/*.find({
 			where: {
 				UserName: req.body.UserName
 			}
@@ -89,7 +89,3 @@ module.exports = {
 		.catch(error => res.status(200).send(error));
 	 },
 }; */
-				
-				
-		
-
--- server/models/user_details.js
+++ server/models/user_details.js
@@ -1,69 +1,69 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User_Details = sequelize.define('User_Details', {
+  const User_Details = sequelize.define("User_Details", {
     FirstName: {
-		type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	LastName: {
-		type: DataTypes.STRING,
-        allowNull: false,
+    LastName: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	StreetAddress: {
-		type: DataTypes.STRING,
-        allowNull: false,
+    StreetAddress: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	City: {
-		type: DataTypes.STRING,
-        allowNull: false,
+    City: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	State: {
-		type: DataTypes.STRING,
-        allowNull: false,
+    State: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	Zipcode: {
-		type: DataTypes.STRING,
-        allowNull: false,
+    Zipcode: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	Password: {
-		type: DataTypes.STRING,
-        allowNull: false,
+    Password: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-	UserName: {
-		type: DataTypes.STRING,
-        allowNull: false,
-		validate: {
-        isUnique: function (value, next) {
+    UserName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUnique: function(value, next) {
           var self = this;
           User_Details.find({ where: { UserName: value } })
-            .then(function (user) {
+            .then(function(user) {
               // reject if a different user wants to use the same username
               if (user && self.id !== user.id) {
-                return next('username already in use!');
+                return next("username already in use!");
               }
               return next();
             })
-            .catch(function (err) {
+            .catch(function(err) {
               return next(err);
             });
         }
       }
-    },
-   });
-   
-  User_Details.associate = (models) => {
+    }
+  });
+
+  User_Details.associate = models => {
     User_Details.hasMany(models.Exercise_Table, {
-      foreignKey: 'userid',
+      foreignKey: "userid"
       //as: 'UserId',
     });
-	User_Details.hasMany(models.User_Badge_Table, {
-      foreignKey: 'userid',
+    User_Details.hasMany(models.User_Badge_Table, {
+      foreignKey: "userid"
       //as: 'UserId',
     });
-	User_Details.hasMany(models.Journal, {
-      foreignKey: 'userid',
+    User_Details.hasMany(models.Journal, {
+      foreignKey: "userid"
       //as: 'UserId',
-    }); 
+    });
   };
   return User_Details;
 };
