b4632da0304aff3d680d0a368de36f44917af90b
Lovedeep <lsingh2@dons.usfca.edu>
2018-10-25 17:37:36 -0700

    getting aal the workouts

--- server/controllers/workoutcategories.js
+++ server/controllers/workoutcategories.js
@@ -4,8 +4,14 @@ const Workout_table = require('../models').Workout_table;
 module.exports = {
   getall(req, res) {
     return workout_categories
-      .findAll()
-      .then(workout_categories => res.status(200).send(workout_categories))
+      .findAll(
+	  {
+		include: [{
+        model: Workout_table,
+        //as: 'Workout_table',
+      }],
+	  })
+	  .then(workout_categories => res.status(200).send(workout_categories))
       .catch(error => res.status(400).send(error));
   },
   retrieve(req, res) {
