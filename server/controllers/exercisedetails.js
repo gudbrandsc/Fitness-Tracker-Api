30b925a1b5e1dc7cfcabb52db04d8e331dd0a802
Lovedeep <lsingh2@dons.usfca.edu>
2018-10-30 14:28:13 -0700

    multiple exercise for user

--- server/controllers/exercisedetails.js
+++ server/controllers/exercisedetails.js
@@ -2,16 +2,25 @@ const exercise_details = require("../models").Exercise_Table;
 
 module.exports = {
   create(req, res) {
-    return exercise_details
+	var inputjsonobject = req.body;
+	var userid = inputjsonobject.userid;
+	var inputjsonarray = inputjsonobject.jsonarray
+	var outputjsonarray;
+	for(var i = 0; i < inputjsonarray.length; i++)
+	{
+	  return exercise_details
       .create({
-        WorkoutId: req.body.WorkoutId,
-        UserId: req.body.UserId,
-        Weight: req.body.Weight,
-        NoOfSets: req.body.NoOfSets,
-        NoOfReps: req.body.NoOfReps
+        WorkoutId: inputjsonarray[i].id,
+        UserId: userid,
+        Weight: 10,
+        NoOfSets: inputjsonarray[i].value1,
+        NoOfReps: inputjsonarray[i].value2,
       })
-      .then(exercise_details => res.status(200).send(exercise_details))
+      .then(exercise_details => outputjsonarray.push(exercise_details))
       .catch(error => res.status(400).send(error));
+	}
+	res.status(200).send(outputjsonarray);
+		
   }
   /*
   listexerciseforuser(req, res) {
