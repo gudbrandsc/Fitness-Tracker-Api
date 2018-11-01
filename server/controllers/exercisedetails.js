851444247b57fc5da7a9a791933b099baff312a8
Lovedeep <lsingh2@dons.usfca.edu>
2018-10-31 23:35:00 -0700

    creating multiple exercises created

--- server/controllers/exercisedetails.js
+++ server/controllers/exercisedetails.js
@@ -2,24 +2,33 @@ const exercise_details = require("../models").Exercise_Table;
 
 module.exports = {
   create(req, res) {
-	var inputjsonobject = req.body;
-	var userid = inputjsonobject.userid;
-	var inputjsonarray = inputjsonobject.workouts
-	var outputjsonarray;
+	const inputjsonobject = req.body;
+	const userid = inputjsonobject.userid;
+	const inputjsonarray = inputjsonobject.workouts;
+	var outputjsonarray = [];
+	console.log("input array lentgth is" +inputjsonarray.length);
 	for(var i = 0; i < inputjsonarray.length; i++)
 	{
-	  return exercise_details
-      .create({
+	  
+      exercise_details.create({
         WorkoutId: inputjsonarray[i].id,
         UserId: userid,
         Weight: 10,
         NoOfSets: inputjsonarray[i].value1,
         NoOfReps: inputjsonarray[i].value2,
       })
-      .then(exercise_details => outputjsonarray.push(exercise_details))
-      .catch(error => res.status(400).send(error));
+      .then(function(exercise_details)
+		{
+		  outputjsonarray.push(exercise_details);
+		  
+		  if(outputjsonarray.length === inputjsonarray.length)
+		  {
+			  res.status(200).send(outputjsonarray);
+		  }
+	  })
+	  .catch(error => res.status(400).send(error));
+	
 	}
-	res.status(200).send(outputjsonarray);
 		
   }
   /*
